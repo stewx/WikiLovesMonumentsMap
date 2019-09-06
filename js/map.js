8c13be13346dc454c355358ad14cde725defc171
Stewart Rand (srand) <stewartrand@gmail.com>
2019-09-06 12:10:43 -0300

    Refactor and add debounce

--- js/map.js
+++ js/map.js
@@ -5,6 +5,21 @@ var idsMapped = [];
 var openInfoWindows = [];
 var markerLimit = 1000;
 
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 function initialize() {
     var mapOptions = {
         center: {
@@ -16,45 +31,48 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     // Bind events
-    google.maps.event.addListener(map, 'bounds_changed', function() {
-        // Get monuments
-        var bounds = map.getBounds();
-        var northeastCoords = bounds.getNorthEast();
-        var southwestCoords = bounds.getSouthWest();
-        var monuments = [];
-
-        $.ajax({
-            url: 'http://tools.wmflabs.org/heritage/api/api.php',
-            method: "GET",
-            dataType: "jsonp",
-            jsonpCallback: 'wlm' + makeid(),
-            data: {
-                action: 'search',
-                //srcountry: 'ca',
-                srlang: 'en',
-                //srmunicipality: 'Halifax',
-                bbox: [southwestCoords.lng(), southwestCoords.lat(), northeastCoords.lng(), northeastCoords.lat()].join(),
-                format: 'json',
-                limit: '500'
-            },
-            timeout: 10000, // 10 second timeout
-            success: function(data) {
-
-                monuments = data.monuments;
-                console.log("Got " + monuments.length + " monuments from API.")
-
-                // Add markers
-                addMarkers(monuments);
-
-                // Clean up old markers if needed
-                cleanupMarkers();
-            }
-        });
-
-    });
+    google.maps.event.addListener(map, 'bounds_changed', debounce(function() {
+        loadMonuments();
+    }, 250));
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function loadMonuments() {
+    // Get monuments
+    var bounds = map.getBounds();
+    var northeastCoords = bounds.getNorthEast();
+    var southwestCoords = bounds.getSouthWest();
+    var monuments = [];
+
+    $.ajax({
+        url: 'http://tools.wmflabs.org/heritage/api/api.php',
+        method: "GET",
+        dataType: "jsonp",
+        jsonpCallback: 'wlm' + makeid(),
+        data: {
+            action: 'search',
+            //srcountry: 'ca',
+            srlang: 'en',
+            //srmunicipality: 'Halifax',
+            bbox: [southwestCoords.lng(), southwestCoords.lat(), northeastCoords.lng(), northeastCoords.lat()].join(),
+            format: 'json',
+            limit: '500'
+        },
+        timeout: 10000, // 10 second timeout
+        success: function(data) {
+
+            monuments = data.monuments;
+            console.log("Got " + monuments.length + " monuments from API.")
+
+            // Add markers
+            addMarkers(monuments);
+
+            // Clean up old markers if needed
+            cleanupMarkers();
+        }
+    });
+}
 
 function addMarkers(monuments) {
     for (var i = 0; i < monuments.length; i++) {
@@ -78,14 +96,14 @@ function addMarkers(monuments) {
                 '<p><a href="' + monument.source + '">View on Wikipedia</a></p>';
             if (monument.country === 'ca') {
                 officialUrl = 'http://www.historicplaces.ca/en/rep-reg/place-lieu.aspx?id=' + monument.id;
-                
+
             } else if (monument.registrant_url) {
                 officialUrl = monument.registrant_url;
             }
             if (officialUrl) {
                 contentString += '<p><a target="_blank" href="' + officialUrl + '">View official listing</a></p>';
             }
-            
+
             var infoWindow = new google.maps.InfoWindow();
             bindInfoWindow(marker, map, infoWindow, contentString);
 
